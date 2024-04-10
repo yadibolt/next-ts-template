947ae9367c45ef45aad6c1d94a3cb0ae5088e05b
Martin Porubcan <mporubcan@activit.sk>
2024-04-10 19:50:38 +0200

    Removed unnecessary packages. Added helmet package for server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 module.exports = app;
